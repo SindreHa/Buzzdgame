6e5881b1d487f2b924ee7a357473af2ebb306ac4
Sindre Haavaldsen <sindre.h@outlook.com>
2020-03-03 18:56:03 +0100

    Header komponent

---
+++ buzzdgame/src/components/Header.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import '../css/header.css';
+
+export default class Header extends Component {
+    
+    bounceAnim() {
+        document.querySelector("h1").classList.add("zoomBounce")
+    };
+
+    componentDidMount() {
+        console.log("Hei");
+    }
+
+    render() {
+        return (
+            <div className="navHeader">
+                <h1 onClick={this.bounceAnim}>BUZZD</h1>
+            </div>
+        )
+    }
+}
