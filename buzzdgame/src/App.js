8e3f217ab428400d7d153a0f6e67e7df8f74dfd7
Sindre Haavaldsen <sindre.h@outlook.com>
2020-03-05 13:15:57 +0100

    Satt opp React Router

--- buzzdgame/src/App.js
+++ buzzdgame/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import RoomCode from './components/RoomCode';
+import ChooseGame from './components/ChooseGame';
+import TextGame1 from './components/TextGame1';
 import Header from './components/Header';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <React.Fragment>
+    <Router>
       <Header />
-      <RoomCode />
-    </React.Fragment>
+      <Route exact path="/" component = {RoomCode}/>
+      <Route path="/choose-game" component = {ChooseGame}/>
+      <Route path="/game" component = {TextGame1}/>
+    </Router>
   );
 }
 
