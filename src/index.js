1d2b6da11cfa7a35b24589aee5d248f1fcf30117
Sindre Haavaldsen <sindre.h@outlook.com>
2020-03-10 20:40:10 +0100

    Funksjonell metode som kalkulerer høyde på faktisk viewport på mobil

--- src/index.js
+++ src/index.js
@@ -5,10 +5,4 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 
-/* Metode som henter høyde av viewport minus nettleser sin toolbar */
-window.addEventListener('resize', () => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  });
-
 ReactDOM.render(<App />, document.getElementById('root'));
