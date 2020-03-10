b21bcd491e3dded21f66c1ae0ee5b6a6f155d6cf
Sindre Haavaldsen <sindre.h@outlook.com>
2020-03-10 20:00:04 +0100

    Småendringer

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import './css/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+/* Metode som henter høyde av viewport minus nettleser sin toolbar */
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+ReactDOM.render(<App />, document.getElementById('root'));
