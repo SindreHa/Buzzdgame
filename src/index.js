e79a8fa7cfb262d937f50f5db5add71ae73dba3f
Sindre Haavaldsen <sindre.h@outlook.com>
2020-03-10 20:08:27 +0100

    Testing

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import * as serviceWorker from './serviceWorker';
 
 
 /* Metode som henter høyde av viewport minus nettleser sin toolbar */
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
 
 ReactDOM.render(<App />, document.getElementById('root'));
