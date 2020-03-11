5b6624e3c3fb960d57a32e42ff8d8ee75bdfe7a6
Sindre Haavaldsen <sindre.h@outlook.com>
2020-03-11 19:19:15 +0100

    Ofte stilte spørsmål og svar - v1

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '../css/faq.css';
+import { Link } from "react-router-dom";
 
 export default class Faq extends Component {
 
@@ -24,44 +25,57 @@ export default class Faq extends Component {
                 },
                 {
                     key: 4,
-                    question: "Lorem iposum?", 
-                    answer: "dolor sit amet, consectetur adipiscing elit. Mauris cursus ullamcorper varius. Maecenas pharetra rutrum urna in porttitor. Aenean sem est"
+                    question: "Er denne siden funksjonell?", 
+                    answer: "Denne siden er fortsatt i beta stadiet og under stadig utvikling. Det vil derfor være flere knapper og funskjoner som det kan hende at ikke fungerer"
                 },
             ]
         }
     }
 
-    expand = (e) => {
-        e.currentTarget.getElementsByClassName("expand-icon")[0].classList.toggle("open")
-        const element = e.currentTarget.parentElement
-        .getElementsByClassName("faq-answer")[0].classList.toggle("collapsed");
-        //element.classList.toggle("open")
-        /* const answer = element.parentElement;
-        console.log(answer.closest("p").className) */
+    componentDidMount() {
+        this.expandListener()
     }
 
-    renderFaq = () => {
-        return (
-            this.state.faq.map((questions) => (
-                <div key={questions.key} className="faqItem">
-                    <p className="faq-question" onClick={this.expand}>
-                        {questions.question}
-                    		<div class="expand-icon">
-                                <span></span>
-                                <span></span>
-                            </div>
-                    </p>
-                    <p className="faq-answer collapsed">{questions.answer}</p>
-                </div>
-            ))
-        )
+    expandListener = () => {
+        var faq = document.getElementsByClassName("faq-question");
+        var i;
+        
+        for (i = 0; i < faq.length; i++) {
+          faq[i].addEventListener("click", function() {
+              console.log("klikk")
+            this.getElementsByClassName("expand-icon")[0].classList.toggle("open");
+            if (this.nextElementSibling.style.maxHeight){
+              this.nextElementSibling.style.maxHeight = null;
+              this.nextElementSibling.style.opacity = 0;
+              this.nextElementSibling.style.padding = 0;
+            } else {
+              this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + "px";
+              this.nextElementSibling.style.opacity = 1;
+              this.nextElementSibling.style.paddingBottom = 10 + "px";
+            } 
+          });
+        }
     }
-
 
     render() {
         return (
         <div className="faqWrapper">
-            {this.renderFaq()}
+        <h2 className="faqHeader">Ofte stilte spørsmål</h2>
+        {
+            this.state.faq.map((questions) => (
+                <div key={questions.key} className="faqItem">
+                    <p className="faq-question" onClick={this.expand}>
+                        {questions.question}
+                        <div className="expand-icon">
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </p>
+                    <p className="faq-answer collapsed">{questions.answer}</p>
+                </div>
+            ))
+        }
+        <Link to="/">Lukk</Link>
         </div>
         )
     }
